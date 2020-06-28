00e5d7064805e881eb238f6f64d8702ddaf6fbc6
vikbert <segentor@gmail.com>
2020-06-28 20:19:19 +0100

    updates

---
+++ S3FileSystemTest.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Chapterphp\Tests;
+
+use Chapterphp\FileSystem\S3FileSystem\S3FileSystem;
+use PHPUnit\Framework\TestCase;
+
+class S3FileSystemTest extends TestCase
+{
+    public function testGet(): void
+    {
+        $this->assertTrue(true);
+    }
+}
--- tests/Model/FileTest.php
+++ tests/Model/FileTest.php
@@ -11,21 +11,31 @@ class FileTest extends TestCase
 {
     public function testGetFileName()
     {
+        //todo:
+        $this->assertTrue(true);
     }
 
     public function testGetFileMimeType()
     {
+        //todo:
+        $this->assertTrue(true);
     }
 
     public function testGetFileLocation()
     {
+        //todo:
+        $this->assertTrue(true);
     }
 
     public function testDelete()
     {
+        //todo:
+        $this->assertTrue(true);
     }
 
     public function testGetFileContent()
     {
+        //todo:
+        $this->assertTrue(true);
     }
 }
