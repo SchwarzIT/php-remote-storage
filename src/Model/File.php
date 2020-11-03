d1f2845ac025c4f97b69a8a68812a3072f99a0d0
vikbert <segentor@gmail.com>
2020-11-03 20:19:19 +0100

    updates

--- src/Model/File.php
+++ src/Model/File.php
@@ -2,7 +2,7 @@
 
 declare(strict_types = 1);
 
-namespace Chapterphp\FileSystem;
+namespace Chapterphp\FileSystem\Model;
 
 use SplFileInfo;
 
--- src/Model/Filename.php
+++ src/Model/Filename.php
@@ -2,8 +2,9 @@
 
 declare(strict_types = 1);
 
-namespace Chapterphp\FileSystem;
+namespace Chapterphp\FileSystem\Model;
 
+use Symfony\Component\Uid\Uuid;
 use Webmozart\Assert\Assert;
 
 class Filename
@@ -12,18 +13,63 @@ class Filename
 
     public function __construct(string $name)
     {
-        Assert::notEmpty($name, 'The parameter $name must not be empty string.');
-
         $this->name = $name;
     }
 
-    public function create(string $name): self
+    public static function create(string $name): self
     {
-        return new self($name);
+        return new self(self::simplify($name));
     }
 
-    public function createUnique(string $name): self
+    public static function createUnique(string $name): self
     {
+        return new self(
+            sprintf(
+                '%s-%s.%s',
+                self::simplify($name),
+                Uuid::v4()->toBase32(),
+                self::getExtension($name)
+            )
+        );
+    }
 
+    /**
+     * @read https://stackoverflow.com/questions/5172261/where-and-why-do-we-use-tostring-in-php
+     */
+    public function toString(): string
+    {
+        return $this->name;
+    }
+
+    private static function simplify(string $name): string
+    {
+        $basename = pathinfo($name, PATHINFO_FILENAME);
+
+        $asciiString = transliterator_transliterate('Any-Latin; Latin-ASCII', $basename);
+        $simplified = preg_replace('/[^A-Za-z0-9_-]/', '_', $asciiString);
+        $simplified = preg_replace('/[_]+/', '_', $simplified);
+
+        Assert::notEmpty($name, sprintf('Invalid parameter $name: "%s"', $name));
+
+        return $simplified;
+    }
+
+    /**
+     * @return string[]
+     */
+    private static function resolveFileSegments(string $name): array
+    {
+        $base = pathinfo($name, PATHINFO_FILENAME);
+        $extension = pathinfo($name, PATHINFO_EXTENSION);
+
+        Assert::notEmpty($base, sprintf('Invalid base name: "%s"', $base));
+        Assert::notEmpty($extension, sprintf('Invalid extension name: "%s"', $extension));
+
+        return [$base, $extension];
+    }
+
+    private static function getExtension(string $name): string
+    {
+        return strtolower(pathinfo($name, PATHINFO_EXTENSION));
     }
 }
